b846d97292e40b62b043f3ddebc1e0fbec608c5a
Chanique Lombard <98527335+Heychani@users.noreply.github.com>
2022-09-12 21:27:02 +0200

    Almost done with the homepage!!

--- js/homepage.js
+++ js/homepage.js
@@ -1,7 +1,152 @@
-new Vue({
-    el: "#app_basic",
-    data: {
-        message: "Hello World",
-        timestamp: `Timestamp ${new Date().toLocaleString()}`,
-    },
+let myVue = new Vue({
+    el: "#netflixApplication",
+
+    movies: [
+        {
+            Title: "Chef's Table Pizza",
+            Genre: "Trending Now",
+            Description: "Dig into the best Pizzas from around the world, prepared by renowned chefs who bake passion, creativity and hard work into every slice",
+            image: "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcTiMViL0x1CSbXYsEnrVb7yIS_s2MBRf0aUiOyGQEGqXT0Y0AUW",
+        },
+
+        {
+            Title: "This is 40",
+            Genre: "Trending Now",
+            Description: "After a big birthday, married couple Pete and Debbie wrestle with realities of parenthood, romance and getting older",
+            image: "https://occ-0-4305-34.1.nflxso.net/dnm/api/v6/tx1O544a9T7n8Z_G12qaboulQQE/AAAABfUJ80d7niqc09vle-s1YHPX-YEvSF0Z6cx6CzDIoSpR_7TS6ixpufkx8wL3saxfU7fjrfwm1yYrr1vbkdA-KdygfrZS8z3N21W0MWBcYP8.webp?r=7a5",
+        },
+
+        {
+            Title: "Buy my house",
+            Genre: "Trending Now",
+            Description: "In this fast-paced reality series, homeowners negotiate with real estate investors in hopes of selling their houses on the spot",
+            image: "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQKV-mLGnUzDeqMclcGm452NpjWGC3Ja2JbXj4lVtvYH3Edkzz3",
+        },
+
+        {
+            Title: "Friends",
+            Genre: "Trending Now",
+            Description: "This hit sitcom follows the merry misadventures of six-something pals as they navigate the pitfalls of work, life and love in 1990s Manhattan.",
+            image: "https://occ-0-4305-34.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABSw3DDSeLkHdqYFndbfOH7P8MDhaHvdr6YxwnfdEDJXNoXg3c-ju2mxCXD3EbZZWPJQsGqO1EBGr8mOiruBTNFy3SzmJBwPDPa0.webp?r=75b",
+        },
+
+        {
+            Title: "The Big Bang Theory",
+            Genre: "Trending Now",
+            Description: "Physicists Leonard and Sheldon find their nerd-centric social circle with pals Howard and Raj expanding when aspiring actress Penny moves in next door",
+            image: "https://occ-0-4305-34.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABTQxxYpzILaZr3UFJp6pabVypB6BMPs7ly5CgL6fIERLjhnC1VvV3dhyEMQPiK8Y_x1vPWLA5TUl-2A1YsQqN7-vM6YkfnKeLl4.webp?r=bdb",
+        },
+
+        {
+            Title: "",
+            Genre: "",
+            Description: "",
+            image: "",
+        },
+
+        {
+            Title: "",
+            Genre: "",
+            Description: "",
+            image: "",
+        },
+
+        {
+            Title: "",
+            Genre: "",
+            Description: "",
+            image: "",
+        },
+
+        {
+            Title: "",
+            Genre: "",
+            Description: "",
+            image: "",
+        },
+
+        {
+            Title: "",
+            Genre: "",
+            Description: "",
+            image: "",
+        },
+
+        {
+            Title: "",
+            Genre: "",
+            Description: "",
+            image: "",
+        },
+
+        {
+            Title: "",
+            Genre: "",
+            Description: "",
+            image: "",
+        },
+
+        {
+            Title: "",
+            Genre: "",
+            Description: "",
+            image: "",
+        },
+
+        {
+            Title: "",
+            Genre: "",
+            Description: "",
+            image: "",
+        },
+
+        {
+            Title: "",
+            Genre: "",
+            Description: "",
+            image: "",
+        },
+
+        {
+            Title: "",
+            Genre: "",
+            Description: "",
+            image: "",
+        },
+
+        {
+            Title: "",
+            Genre: "",
+            Description: "",
+            image: "",
+        },
+
+        {
+            Title: "",
+            Genre: "",
+            Description: "",
+            image: "",
+        },
+
+        {
+            Title: "",
+            Genre: "",
+            Description: "",
+            image: "",
+        },
+
+        {
+            Title: "",
+            Genre: "",
+            Description: "",
+            image: "",
+        },
+
+        {
+            Title: "",
+            Genre: "",
+            Description: "",
+            image: "",
+        }
+    ]
 })
